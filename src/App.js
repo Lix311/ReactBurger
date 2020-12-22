cb3c3f6653658ae13be9f7c9d291d927f2265103
Lix311 <alexsterrantino@gmail.com>
2020-12-21 20:51:10 -0500

    added props

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ class App extends Component {
       <div className="App">
         <h1>Hi Im a react app</h1>
         <h2>its really working</h2>
-        <Person />
+        <Person name="Alex" age="30" />
+        <Person name="Max" age="30"/>
+        <Person name="Lauren" age="60"/>
+        <Person name="Peter" age="60"/>
       </div>
     );
   }
