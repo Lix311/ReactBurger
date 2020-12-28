96d62a604579188d4b9d16ee13c7c679d58b6621
Lix311 <alexsterrantino@gmail.com>
2020-12-27 19:00:21 -0500

    mapping lists

--- src/App.js
+++ src/App.js
@@ -51,17 +51,12 @@ class App extends Component {
     if (this.state.showPersons){
       persons = (
       <div>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age} 
-          click={this.switchNameHandler.bind(this, 'Alex')}/>
-        <Person 
-          changed={this.switchPersonsName}
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age} />
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age} />
+        {this.state.persons.map(person => {
+          return <Person 
+          name={person.name} 
+          age={person.age}
+          />
+        })}
       </div>
       );
     }
